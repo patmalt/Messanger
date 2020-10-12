06a296078bc0ed202d386322fc4dd8ec76d1c2da
Patrick Maltagliati <patmalt@gmail.com>
2020-10-12 09:48:44 -0500

    Move view modifier into its own file.

--- Messanger/Application/ContentView.swift
+++ Messanger/Application/ContentView.swift
@@ -57,28 +57,3 @@ struct ContentView: View {
     
     private func newMessageButtonAction() { isShowingNewMessageView.toggle() }
 }
-
-
-
-
-
-struct AnimatableSystemFontModifier: AnimatableModifier {
-    var size: CGFloat
-    var weight: Font.Weight
-    var design: Font.Design
-
-    var animatableData: CGFloat {
-        get { size }
-        set { size = newValue }
-    }
-
-    func body(content: Content) -> some View {
-        content.font(.system(size: size, weight: weight, design: design))
-    }
-}
-
-extension View {
-    func animatableSystemFont(size: CGFloat, weight: Font.Weight = .regular, design: Font.Design = .default) -> some View {
-        modifier(AnimatableSystemFontModifier(size: size, weight: weight, design: design))
-    }
-}
